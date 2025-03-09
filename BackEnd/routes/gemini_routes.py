0612cb9c67b3242e42784c7b0b9aa8d9c238b7b8
Elijah <elijah.olson123@gmail.com>
2025-03-09 00:24:00 -0800

    done

--- BackEnd/routes/gemini_routes.py
+++ BackEnd/routes/gemini_routes.py
@@ -107,7 +107,7 @@ def generateGeminiResponse():
     # Combine the contents of both files
     combined_input = (
         "In the following prompt, you are an expert course designer and educator creating a comprehensive course on: " + user_query + " using the following format" + structure_template + "\n"
-        "1. **Course Title**: A clear and concise course title."
+        "1. **Course Title**: A clear and concise course title, no more than 3 words, descript about the subject of study"
         "2. **Prerequisites**: The second section of this course should outline all the pre-existing knowledge one needs prior to learning the material they asked about (for example one needs to know algebra to pursue calculus, or have taken physics before doing some level of dynamic engineering)\n"
         "3. **Course Content**: The third section should be a complete layout of the material that will be covered relating to their prompt and course they are asking about\n"
         "4. **Resources**: The fourth section should be one that provides the user with resources for each part of the covered material, containing attached links. This is where you factor in the User preferences I above mentioned. Based on the user's preferences, you are to suggest different medias for learning they may enjoy more, or benefit from greater. You should provide them with a plethora of resources, and majorly ones that fit within their preferences.\n"
